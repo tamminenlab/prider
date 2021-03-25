68b7fe8134fc35f6071302640764ebd3687e9818
Manu Tamminen <mavata@gmail.com>
2021-03-25 11:51:38 +0200

    Chunker improvements

--- src/chunker.cpp
+++ src/chunker.cpp
@@ -20,23 +20,28 @@ std::vector< std::string > sliding_window(std::string sequence, int window_size)
 DataFrame chunker(DataFrame seq_table,
                   int window_size = 20) 
 {
-
   std::vector< std::string > ids = seq_table["Id"];
   std::vector< std::string > seqs = seq_table["Seq"];
   
   std::vector< std::string > seq_chunk_acc;
   std::vector< std::string > seq_id_acc;
+  std::vector< std::string > seq_ix_acc;
+  std::vector< std::string > seq_id_ix_acc;
 
   for (int inc1 = 0; inc1 < seqs.size(); ++inc1) {
     std::vector< std::string > seq_acc(sliding_window(seqs[inc1], window_size));
     std::string seq_id = ids[inc1];
     for (int inc2 = 0 ; inc2 < seq_acc.size() ; ++inc2) {
       seq_chunk_acc.push_back(seq_acc[inc2]);
-      seq_id_acc.push_back(seq_id + "_" + std::to_string(inc2 + 1));
+      seq_id_acc.push_back(seq_id);
+      seq_ix_acc.push_back(std::to_string(inc2 + 1));
+      seq_id_ix_acc.push_back(seq_id + "_" + std::to_string(inc2 + 1));
     }
   }
 
   return DataFrame::create(Named("Id") = seq_id_acc,
+                           Named("Ix") = seq_ix_acc,
+                           Named("Id_Ix") = seq_id_ix_acc,
                            Named("Seq") = seq_chunk_acc);
 }
 
